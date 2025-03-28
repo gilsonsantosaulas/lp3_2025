a55dfb727ae80416f982fe1dc83730196af492d4
gilsonsatu <gilsonsatu@gmail.com>
2025-03-28 11:03:21 -0400

    comentarios em se01

--- SE01/se01.js
+++ SE01/se01.js
@@ -1,15 +1,22 @@
+//editor online: https://jsfiddle.net/
+
+//no editor online exclua a linha abaixo
 const rl = require('readline-sync');
 
 let qtd = 4;
 
 let gabarito = [];
 for (let i = 0; i < qtd; i++) {
+    //no editor online use:
+    //prompt("Acertos: "+acertos);
     resposta = rl.question("Gabarito: ");
     gabarito[i] = resposta;
 }
 
 let respostasAluno = [];
 for (let i = 0; i < qtd; i++) {
+    //no editor online use:
+    //prompt("Acertos: "+acertos);
     resposta = rl.question("Respostas: ");
     respostasAluno[i] = resposta;
 }
@@ -17,10 +24,11 @@ for (let i = 0; i < qtd; i++) {
 
 let acertos = 0;
 for (let i = 0; i < qtd; i++) {
-    if (respostasAluno[i] === gabarito[i]) {
+    if (respostasAluno[i] == gabarito[i]) {
         acertos++;
     }
 }
-
+//no editor online use:
+//alert("Acertos: "+acertos);
 console.log("Acertos: "+acertos);
 
