1885bd7195bd2f42e48a71c2c3016bedf311d51b
gilsonsatu <gilsonsatu@gmail.com>
2025-05-16 11:31:56 -0400

    Exemplo Slide 2

--- ExemploSlide2/ExemploSlide2.js
+++ ExemploSlide2/ExemploSlide2.js
@@ -4,8 +4,14 @@ function somarElementosVetor(vetor) {
       soma = soma+vetor[i];
     }
     return soma; // retorna 10
-  }
-  let vetorNumeros = [1, 2, 3, 4];
-  let resultado = somarElementosVetor(vetorNumeros);
-  console.log(resultado); // mostra 10
+}
+let vetorNumeros = [1, 2, 3, 4];
+let resultado = somarElementosVetor(vetorNumeros);
+console.log(resultado); // mostra 10
+
+let vetorNumeros2 = [10, 2, 300, 40];
+console.log(somarElementosVetor(vetorNumeros2))
+
+let vetorNumeros3 = [10, 100, 1000];
+console.log(somarElementosVetor(vetorNumeros3));
   
